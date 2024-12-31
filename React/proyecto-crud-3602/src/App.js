7929f25a886d835aeb2063e79e6f7e0c07ae6726
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2024-12-31 04:12:41 -0600

    Actualizar App.js

--- React/proyecto-crud-3602/src/App.js
+++ React/proyecto-crud-3602/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';  
 import Formulario from './components/Formulario'; 
 
-const App = () => {  
+const App = () => {   
     return ( 
         <div> 
             <Formulario />
