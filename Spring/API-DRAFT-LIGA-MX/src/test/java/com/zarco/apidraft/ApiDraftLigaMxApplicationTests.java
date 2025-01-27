5842c989d085a5122ed44ae225355d55b78902c3
Angel Zarco <91713291+angel-manuel-zarco@users.noreply.github.com>
2025-01-26 22:02:43 -0600

    Actualizar ApiDraftLigaMxApplicationTests.java

--- Spring/API-DRAFT-LIGA-MX/src/test/java/com/zarco/apidraft/ApiDraftLigaMxApplicationTests.java
+++ Spring/API-DRAFT-LIGA-MX/src/test/java/com/zarco/apidraft/ApiDraftLigaMxApplicationTests.java
@@ -3,7 +3,7 @@ package com.zarco.apidraft;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest; 
  
-@SpringBootTest
+@SpringBootTest 
 class ApiDraftLigaMxApplicationTests {
 
 	@Test
